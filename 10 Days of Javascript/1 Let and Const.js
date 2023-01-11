b77069b460e4576da28cc4569c9d1e29d8e367c5
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-01-11 18:33:53 +0800

    Create 1 Let and Const.js

---
+++ 10 Days of Javascript/1 Let and Const.js
@@ -0,0 +1,8 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r = readLine()
+    const PI=Math.PI
+    // Print the area of the circle:
+    console.log(Math.pow(r,2)*PI);
+    // Print the perimeter of the circle:
+    console.log(2*r*PI);
