f885b4559ada035ecffe291a17eee56c09a63196
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-01-16 19:36:51 +0800

    Create 4 Classes.js

---
+++ 10 Days of Javascript/4 Classes.js
@@ -0,0 +1,23 @@
+class Polygon{
+    constructor(sideLength){
+        this.sideLength=sideLength
+    }
+    perimeter(){
+        let perimeter=0
+        for(let side of this.sideLength){
+            perimeter=perimeter+ side
+        }
+        return perimeter
+    }
+    
+    
+}
+
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
