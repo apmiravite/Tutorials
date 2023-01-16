9aff68b4e43a0f5dde78aaae449cccf08594adae
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-01-16 18:36:59 +0800

    Create 4a Create a rectangle object.js

---
+++ 10 Days of Javascript/4a Create a rectangle object.js
@@ -0,0 +1,11 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    var length=a
+    var width=b
+    var perimeter=2*(length+width)
+    var area=length*width
+    
+    return{length, width, perimeter, area}
+}
